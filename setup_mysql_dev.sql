71fb24aad550646e0b34f2124ec81fae8f47c69e
kaytee07 <kofi.taylor07@gmail.com>
2023-06-20 11:28:34 +0000

    CREATE USERS AND GRANT PRIVELEGES

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- prepare MySQL server for AirBnB
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVELEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev';
+GRANT ALL PRIVELEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVELEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- prepare MySQL server for AirBnB
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVELEGES ON 'hbnb_test_db'.* TO 'hbnb_test';
-GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test';
+GRANT ALL PRIVELEGES ON 'hbnb_test_db'.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVELEGES;
